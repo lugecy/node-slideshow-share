fc83c639695d9752e9f4e3c9991117017e399490
lugecy <lugecy@gmail.com>
2014-10-05 16:13:51 +0900

    Refactoring: class定義テンプレートに従った書き方に変更

--- slideshow-share-client.js
+++ slideshow-share-client.js
@@ -1,47 +1,60 @@
-var SlideShow = function(url_list, body_id) {
-	this.body_id = body_id;
-	this.image_list = [];
-	this.current_idx = 0;
-	var css_classname = "slideshow-image";
+(function (window, document, undefined) {
+	var www = window.www = window.www || {};
+	www.SlideShow = (function () {
+		//Class Private member
+		var css_classname = "slideshow-image";
 
-	this.show_image = function (img) {
-		img.className = css_classname + " active";
-	}
-	this.hide_image = function (img) {
-		img.className = css_classname;
-	}
-
-	for (var idx = 0; idx < url_list.length; idx++) {
-		var image_info = url_list[idx];
-		var img = new Image();
-		img.src = image_info.url;
-		img.className = css_classname;
-		this.image_list.push(img);
-	}
-
-	var body = document.getElementById(body_id);
-	for (var idx = 0; idx < this.image_list.length; idx++) {
-		var img = this.image_list[idx];
-		if (idx === 0) {
-			this.show_image(img);
+		//Private methods
+		function show_image(img) {
+			img.className = css_classname + " active";
 		}
-		body.appendChild(img);
-	}
 
-};
+		function hide_image(img) {
+			img.className = css_classname;
+		}
 
-SlideShow.prototype.go = function (forward_idx) {
-	var next_idx = this.current_idx + forward_idx;
-	//インデックスの範囲をチェック
-	if (next_idx < 0 || next_idx >= this.image_list.length) {
-		return;
-	}
-	//現在の画像を隠す
-	var curret_image = this.image_list[this.current_idx];
-	this.hide_image(curret_image);
-	//次の画像を表示
-	var next_image = this.image_list[next_idx];
-	this.show_image(next_image);
-	//インデックス更新
-	this.current_idx = next_idx;
-}
+		//Constructer
+		function SlideShow (url_list, body_id) {
+			this._image_list = [];
+			this._current_idx = 0;
+			//URLリストよりimgタグリストを生成
+			for (var idx = 0; idx < url_list.length; idx++) {
+				var image_info = url_list[idx];
+				var img = new Image();
+				img.src = image_info.url;
+				img.className = css_classname;
+				this._image_list.push(img);
+			}
+			//スライド表示用領域に画像リストを挿入
+			var body = document.getElementById(body_id);
+			for (var idx = 0; idx < this._image_list.length; idx++) {
+				var img = this._image_list[idx];
+				if (idx === 0) {
+					show_image(img);
+				}
+				body.appendChild(img);
+			}
+		}
+
+		//Public methods
+		var p = SlideShow.prototype;
+
+		p.go = function (forward_idx) {
+			var next_idx = this._current_idx + forward_idx;
+			//インデックスの範囲をチェック
+			if (next_idx < 0 || next_idx >= this._image_list.length) {
+				return;
+			}
+			//現在の画像を隠す
+			var curret_image = this._image_list[this._current_idx];
+			hide_image(curret_image);
+			//次の画像を表示
+			var next_image = this._image_list[next_idx];
+			show_image(next_image);
+			//インデックス更新
+			this._current_idx = next_idx;
+		};
+
+		return SlideShow;
+	}());
+}(window, window.document));
